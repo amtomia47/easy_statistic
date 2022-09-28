7d36d9fc5b8fd23fdf1611ab99fa7892b15c638f
amtomia47 <81173196+amtomia47@users.noreply.github.com>
2022-09-28 14:12:51 +0800

    Final 9/28

--- php/delete_all.php
+++ php/delete_all.php
@@ -1,3 +1,4 @@
 <?php
     $mysqli = new mysqli('localhost','root','','student_data');
+    $mysqli->query("UPDATE `student_count` SET `count` = 0");    
     $mysqli->query("DELETE FROM `student_data`");
